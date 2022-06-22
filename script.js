bab1514b3fe9b0dc91d3ce63e2f0cab3c2bb7567
Renato <rkauric@vvg.hr>
2022-06-22 20:32:05 +0200

    Revisited and finished this game in new branch.

--- script.js
+++ script.js
@@ -1,81 +1,76 @@
-let myArray = [
-    'Rock',
-    'Paper',
-    'Scissors'
-];
-
 let pcStreak = 0;
 let myStreak = 0;
 
-function playRound() {
+const buttons = document.querySelectorAll(".button");
 
-    /* Only part of this game that I had to google for.
-    Didn't know how to randomly choose from array. */
-    let computerGuess = myArray[Math.floor(Math.random() * myArray.length)];
-    let userGuess = prompt("Choose from 'Rock', 'Paper' or 'Scissors'.");
+buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+        const userGuess = button.id;    
 
-    if (computerGuess == 'Rock') {
+        const cOptions = ["Rock", "Paper", "Scissors"];
+        const computerGuess = cOptions[Math.floor(Math.random() * 3)];
 
-        if (userGuess.toLowerCase() == 'rock') {
-            console.log(`It's a tie! You choose ${userGuess} while PC choose ${computerGuess}.`)
+        playRound(userGuess, computerGuess);
+    });
+
+    function playRound(userGuess, computerGuess) {
+
+        if (computerGuess == 'Rock') {
+
+            if (userGuess.toLowerCase() == 'rock') {
+                document.getElementById("55").innerHTML =(`It's a tie! You choose ${userGuess} while PC choose ${computerGuess}.`)
+            }
+
+            else if (userGuess.toLowerCase() == 'scissors') {
+                document.getElementById("55").innerHTML =(`You lost! You choose ${userGuess} while PC choose ${computerGuess}.`)
+                pcStreak++;
+                document.getElementById("2").innerHTML = `PC: ${pcStreak}`;
+            }
+
+            else {
+                document.getElementById("55").innerHTML =(`You won! You choose ${userGuess} while PC choose ${computerGuess}.`)
+                myStreak++;
+                document.getElementById("1").innerHTML = `Me: ${myStreak}`;
+            }
         }
 
-        else if (userGuess.toLowerCase() == 'scissors') {
-            console.log(`You lost! You choose ${userGuess} while PC choose ${computerGuess}.`)
-            pcStreak++;
-            document.getElementById("2").innerHTML = `PC: ${pcStreak}`;
-        }
+        else if (computerGuess == 'Paper') {
 
-        else {
-            console.log(`You won! You choose ${userGuess} while PC choose ${computerGuess}.`)
-            myStreak++;
-            document.getElementById("1").innerHTML = `Me: ${myStreak}`;
-        }
-    }
+            if (userGuess.toLowerCase() == 'paper') {
+                document.getElementById("55").innerHTML =(`It's a tie! You choose ${userGuess} while PC choose ${computerGuess}.`)
+            }
 
-    else if (computerGuess == 'Paper') {
+            else if (userGuess.toLowerCase() == 'scissors') {
+                document.getElementById("55").innerHTML =(`You won! You choose ${userGuess} while PC choose ${computerGuess}.`)
+                myStreak++;
+                document.getElementById("1").innerHTML = `Me: ${myStreak}`;
+            }
 
-        if (userGuess.toLowerCase() == 'paper') {
-            console.log(`It's a tie! You choose ${userGuess} while PC choose ${computerGuess}.`)
-        }
-
-        else if (userGuess.toLowerCase() == 'scissors') {
-            console.log(`You won! You choose ${userGuess} while PC choose ${computerGuess}.`)
-            myStreak++;
-            document.getElementById("1").innerHTML = `Me: ${myStreak}`;
-        }
-
-        else {
-            console.log(`You lost! You choose ${userGuess} while PC choose ${computerGuess}.`)
-            pcStreak++;
-            document.getElementById("2").innerHTML = `PC: ${pcStreak}`;
-        }
-    }
-
-    else {
-        if (userGuess.toLowerCase() == 'paper') {
-
-            console.log(`You lost! You choose ${userGuess} while PC choose ${computerGuess}.`)
-            pcStreak++;
-            document.getElementById("2").innerHTML = `PC: ${pcStreak}`;
-        }
-
-        else if (userGuess.toLowerCase() == 'scissors') {
-            console.log(`It's a tie! You choose ${userGuess} while PC choose ${computerGuess}.`)
+            else {
+                document.getElementById("55").innerHTML =(`You lost! You choose ${userGuess} while PC choose ${computerGuess}.`)
+                pcStreak++;
+                document.getElementById("2").innerHTML = `PC: ${pcStreak}`;
+            }
         }
 
         else {
-            console.log(`You won! You choose ${userGuess} while PC choose ${computerGuess}.`)
-            myStreak++;
-            document.getElementById("1").innerHTML = `Me: ${myStreak}`;
+            if (userGuess.toLowerCase() == 'paper') {
 
-        }
-    }
+                document.getElementById("55").innerHTML =(`You lost! You choose ${userGuess} while PC choose ${computerGuess}.`)
+                pcStreak++;
+                document.getElementById("2").innerHTML = `PC: ${pcStreak}`;
+            }
 
-}
+            else if (userGuess.toLowerCase() == 'scissors') {
+                document.getElementById("55").innerHTML =(`It's a tie! You choose ${userGuess} while PC choose ${computerGuess}.`)
+            }
 
-function game() {
-    for (let i = 0; i < 5; i++){
-        playRound()
-    }
-}
+            else {
+                document.getElementById("55").innerHTML =(`You won! You choose ${userGuess} while PC choose ${computerGuess}.`)
+                myStreak++;
+                document.getElementById("1").innerHTML = `Me: ${myStreak}`;
+
+            };
+        };
+    };
+});
